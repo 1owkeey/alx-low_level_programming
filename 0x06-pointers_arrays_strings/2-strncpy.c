9bf46846a42b45cac8a029be7270aea0ef067323
1owkeey <luckyayuba043@gmail.com>
2023-09-21 19:29:43 -0700

    C-more pointers

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy -do copy a string
+ * @dest:do input value
+ * @srcdo:do input value
+ * @n:do input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
