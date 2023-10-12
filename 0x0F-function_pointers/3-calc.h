e16f4325704632d961d0976d068e6962704bf8f2
1owkeey <luckyayuba043@gmail.com>
2023-10-11 18:13:02 -0700

    C-Functions_pointers

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,21 @@
+#ifndef CALC_H
+#define CALC_H
+/**
+ * struct op - do a struct op.
+ * @op: as in the operator.
+ * @f: as in the associated function.
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
